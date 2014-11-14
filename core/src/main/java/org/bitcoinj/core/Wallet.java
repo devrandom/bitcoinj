f1a95602576247f0637a1b51ecfbb57779a8324b
Devrandom <c1.devrandom@niftybox.net>
2014-11-14 11:44:09 -0800

    speed up wallet creation by moving lookahead out

--- core/src/main/java/org/bitcoinj/core/Wallet.java
+++ core/src/main/java/org/bitcoinj/core/Wallet.java
@@ -247,8 +247,10 @@ public class Wallet extends BaseTaggableObject implements Serializable, BlockCha
         // If this keychain was created fresh just now (new wallet), make HD so a backup can be made immediately
         // without having to call current/freshReceiveKey. If there are already keys in the chain of any kind then
         // we're probably being deserialized so leave things alone: the API user can upgrade later.
-        if (this.keychain.numKeys() == 0)
-            this.keychain.createAndActivateNewHDChain();
+
+        // Not needed for normal use, and slows down startup
+//        if (this.keychain.numKeys() == 0)
+//            this.keychain.createAndActivateNewHDChain();
         watchedScripts = Sets.newHashSet();
         unspent = new HashMap<Sha256Hash, Transaction>();
         spent = new HashMap<Sha256Hash, Transaction>();
